2f4b7d6e9956d8d22843060d171905e9a00be8da
scole <coles2@hartwick.edu>
2020-02-11 10:00:23 -0500

    I tried a little bit again

--- ListAssignment.py
+++ ListAssignment.py
@@ -1,31 +1,31 @@
 # Sam Cole
 # 2/10/2020
 
-
+# finds the sum of the list
 def sumlist(numbers):
+    total = 0
     for number in numbers:
-        total = 0
         total = number + total
-    return f"The sum of the list is {total}"
+    return total
 
-
+# finds the product of the list
 def productlist(numbers):
+    product = 1
     for number in numbers:
-        product = 1
         product = number * product
-    return f"The product of the list is {product}"
+    return product
 
-
+# finds the mean of the list
 def meanlist(numbers):
-    total =
+    total = sumlist(numbers)
     mean = total / 5
-    return f"the mean is {mean}"
+    return mean
 
-
+# finds the median of the list
 def medainlist(numbers):
-    return f"The median of the list is {numbers[2]}"
+    return numbers[2]
 
-
+# finds the mode of the list
 def modelist(numbers):
     if numbers[0] == numbers[1] or numbers[2] or numbers[3] or numbers[4]:
         mode = numbers[0]
@@ -38,3 +38,64 @@ def modelist(numbers):
     else:
         return "There is no mode sorry"
     return f"The mode is {mode}"
+
+# finds the largest number in the list
+def largestnum(numbers):
+    return numbers[4]
+
+# finds the smallest number in the list
+def smallestnum(numbers):
+    return numbers[0]
+
+# deletes the duplicates in a list
+def noduplicates(numbers):
+    if numbers[0] == numbers[1]:
+        del numbers[0]
+    elif numbers[0] == numbers[2]:
+        del numbers[0]
+    elif numbers[0] == numbers[3]:
+        del numbers[0]
+    elif numbers[0] == numbers[4]:
+        del numbers[0]
+    elif numbers[1] == numbers[2]:
+        del numbers[1]
+    elif numbers[1] == numbers[3]:
+        del numbers[1]
+    elif numbers[1] == numbers[4]:
+        del numbers[1]
+    elif numbers[2] == numbers[3]:
+        del numbers[2]
+    elif numbers[2] == numbers[4]:
+        del numbers[2]
+    elif numbers[3] == numbers[4]:
+        del numbers[3]
+    else:
+        return "There is no duplicates"
+    return numbers
+
+# deletes the even numbers in a list
+def noeven(numbers):
+    count = 0
+    for number in numbers:
+        if number % 2 == 0:
+            del numbers[count]
+        count = count + 1
+    return numbers
+
+# deletes the odd numbers in a list
+def noodd(numbers):
+    count = 0
+    for number in numbers:
+        if number % 2 != 0:
+            numbers.remove(number)
+            #del numbers[count]
+        count = count + 1
+    return numbers
+
+# checks to see if a number is in the list
+def isitthere(numbers, guess):
+    for number in numbers:
+        if guess == number:
+            return "Its in there alright"
+        else:
+            return "Sorry not in there"
--- Runner.py
+++ Runner.py
@@ -1,7 +1,23 @@
 # Sam Cole
 # This is the runner
 import ListAssignment
-numbers = [5, 6, 7, 8, 9]
+numbers = [5, 6, 7, 5, 9]
 
 numbers.sort()
-print()
+print(numbers)
+print(f"ListAssignment.sumlist(numbers))
+print(f"The product of the list is {ListAssignment.productlist(numbers)}")
+print(f"The mean is {ListAssignment.meanlist(numbers)}")
+print(f"The median is {ListAssignment.medainlist(numbers)}")
+print(ListAssignment.modelist(numbers))
+numbers2 = list(numbers)
+print("There are no more duplicates")
+print(ListAssignment.noduplicates(numbers2))
+numbers2 = list(numbers)
+print("I deleted all the even numbers")
+print(ListAssignment.noeven(numbers2))
+numbers2 = list(numbers)
+print("I deleted all the odd numbers")
+print(ListAssignment.noodd(numbers2))
+guess = input("Type a number to see if its in the list")
+print(ListAssignment.isitthere(numbers, guess))
